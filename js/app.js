75519ce1d2d00e07a4d820ab0fa5e9cc8afaae89
Luisc013 <c.luis013@gmail.com>
2020-12-15 21:57:28 -0500

    Made the input value accessible and created a onclick function for the submit button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-
-
-
-
+function submitMessage() {
+    const button = document.querySelector(".btn");
+    let form = document.getElementById("message-form");
+    button.addEventListener("click", () => {
+        
+    })
+}
