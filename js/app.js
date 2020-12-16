8c78f047a700e67dbbf49f875668342b1fead652
Luisc013 <c.luis013@gmail.com>
2020-12-16 13:37:33 -0500

    Message is being displayed but ony for a milisecond and then reverts back to initial msg

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
-function submitMessage() {
-    const button = document.querySelector(".btn");
-    let form = document.getElementById("message-form");
-    button.addEventListener("click", () => {
-        
+    const form = document.getElementById("message-form")
+    form.addEventListener('submit', () => {
+        const message = document.getElementById('message')
+        const feedback = document.querySelector('.feedback')
+        const messageContent = document.querySelector('.message-content')
+        if (message.value === ''){
+            feedback.classList.add('show')
+        } else {
+            messageContent.textContent = message.value
+             if(feedback.classList.contains('show')){
+                feedback.classList.remove('show')
+             }
+             message.value = '';
+        }
     })
-}
